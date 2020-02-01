80cdec47b44dbedfd4044d11e16496c1ad700a5c
Hein Dauven <heindauven@gmail.com>
2020-02-02 00:31:17 +0100

    Add logging to CLI module.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,7 +1,11 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-agent", "Elixir user@user.com"}]
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -12,11 +16,10 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body |> Poison.Parser.parse!()}
-  end
-
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code =#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error(),
       body |> Poison.Parser.parse!()

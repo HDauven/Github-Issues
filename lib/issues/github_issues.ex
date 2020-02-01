7e4644a3911be402c775e15be613c37e74495fb7
Hein Dauven <heindauven@gmail.com>
2020-02-01 16:48:14 +0100

    Check for errors and parse body of failed github requests.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -7,8 +7,9 @@ defmodule Issues.GithubIssues do
     |> handle_response
   end
 
+  @github_url Application.get_env(:issues, :github_url)
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
@@ -16,6 +17,10 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
+    status_code |> check_for_error(),
+    body        |> Poison.Parser.parse!()
   end
+
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_),   do: :error
 end
